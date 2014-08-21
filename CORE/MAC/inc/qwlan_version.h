2c14e8fbf0112df88e2ce6f180d8c1d5fe69691a
Bansidhar Gopalachari <bansid@codeaurora.org>
2014-08-21 14:51:49 -0700

    SBA 4.3.6.105 on top of 4.3.6.104
    Change-Id: Ieba1ddc13e26b9d5bb3889eb9c7d98b93835907b
    Reviewed-on: http://gerrit.mot.com/665827
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    SLTApproved: Binesh Balasingh <binesh@motorola.com>

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            3
 #define QWLAN_VERSION_PATCH            6
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            104
+#define QWLAN_VERSION_BUILD            105
 
-#define QWLAN_VERSIONSTR               "4.3.6.104"
+#define QWLAN_VERSIONSTR               "4.3.6.105"
 
 #ifdef QCA_WIFI_2_0
 
