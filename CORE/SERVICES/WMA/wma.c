cce6f0471a6abc6175f0ebc5830579d1eedc0bbf
Bansidhar Gopalachari <bansid@codeaurora.org>
2014-08-22 18:37:22 -0700

    SBA 4.3.6.107 on top of 4.3.6.106
    Change-Id: I5359079cf0d48da67079c63f37b7d84a9dc68c22
    Reviewed-on: http://gerrit.mot.com/665828
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    SLTApproved: Binesh Balasingh <binesh@motorola.com>

--- CORE/SERVICES/WMA/wma.c
+++ CORE/SERVICES/WMA/wma.c
@@ -14056,6 +14056,8 @@ int wma_enable_wow_in_fw(WMA_HANDLE handle)
 	struct ol_softc *scn;
 	int host_credits;
 	int wmi_pending_cmds;
+	tpAniSirGlobal pMac = (tpAniSirGlobal)vos_get_context(VOS_MODULE_ID_PE,
+				wma->vos_context);
 
 	len = sizeof(wmi_wow_enable_cmd_fixed_param);
 
@@ -14105,8 +14107,14 @@ int wma_enable_wow_in_fw(WMA_HANDLE handle)
 		WMA_LOGE("Credits:%d; Pending_Cmds: %d",
 			wmi_get_host_credits(wma->wmi_handle),
 			wmi_get_pending_cmds(wma->wmi_handle));
-
+#ifdef CONFIG_CNSS
+		if (pMac->sme.enableSelfRecovery) {
+			vos_set_logp_in_progress(VOS_MODULE_ID_HIF, TRUE);
+			cnss_schedule_recovery_work();
+		}
+#else
 		VOS_BUG(0);
+#endif
 		wmi_set_target_suspend(wma->wmi_handle, FALSE);
 		return VOS_STATUS_E_FAILURE;
 	}
@@ -14876,6 +14884,8 @@ static VOS_STATUS wma_send_host_wakeup_ind_to_fw(tp_wma_handle wma)
 	VOS_STATUS vos_status = VOS_STATUS_SUCCESS;
 	int32_t len;
 	int ret;
+	tpAniSirGlobal pMac = (tpAniSirGlobal)vos_get_context(VOS_MODULE_ID_PE,
+				wma->vos_context);
 
 	len = sizeof(wmi_wow_hostwakeup_from_sleep_cmd_fixed_param);
 
@@ -14911,10 +14921,19 @@ static VOS_STATUS wma_send_host_wakeup_ind_to_fw(tp_wma_handle wma)
 		WMA_LOGP("%s: Pending commands %d credits %d", __func__,
 				wmi_get_pending_cmds(wma->wmi_handle),
 				wmi_get_host_credits(wma->wmi_handle));
-		if (!vos_is_logp_in_progress(VOS_MODULE_ID_HIF, NULL))
+		if (!vos_is_logp_in_progress(VOS_MODULE_ID_HIF, NULL)) {
+#ifdef CONFIG_CNSS
+			if (pMac->sme.enableSelfRecovery) {
+				vos_set_logp_in_progress(VOS_MODULE_ID_HIF,
+							TRUE);
+				cnss_schedule_recovery_work();
+			}
+#else
 			VOS_BUG(0);
-		else
+#endif
+		} else {
 			WMA_LOGE("%s: SSR in progress, ignore resume timeout", __func__);
+		}
 	} else {
 		WMA_LOGD("Host wakeup received");
 	}
