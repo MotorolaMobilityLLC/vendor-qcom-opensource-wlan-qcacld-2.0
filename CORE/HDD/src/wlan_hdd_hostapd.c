6b22ad331b0a38959857f62639188b23cf4cf18a
Binesh B <binesh@motorola.com>
2015-04-16 17:02:35 +0530

    WLAN - Clark - Enable extended iwpriv commands
    Enable hostapd ioctl handling fore new iwpriv range added in Rome 2 driver
    
    Change-Id: I91fd3ae1f9c5b1084c92d7008e53c829430d0f6d
    Reviewed-on: http://gerrit.mot.com/735680
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Prashanth Chakravarthy <a19110@motorola.com>
    SLTApproved: Binesh Balasingh <binesh@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- CORE/HDD/src/wlan_hdd_hostapd.c
+++ CORE/HDD/src/wlan_hdd_hostapd.c
@@ -1969,7 +1969,7 @@ static iw_softap_setparam(struct net_device *dev,
 
 
     //BEGIN MOT a19110 IKDREL3KK-11113 Fix iwpriv panic
-    if(tmp[0] < 0 || tmp[0] > QCASAP_SET_TM_LEVEL)
+    if(tmp[0] < 0 || tmp[0] > QCASAP_SET_PHYMODE)
     {
         value = (int *)(wrqu->data.pointer);
     }
