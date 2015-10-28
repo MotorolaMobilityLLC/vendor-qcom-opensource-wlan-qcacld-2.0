3c69cd4c1111c66a714bc8982bb67bc8434d0375
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2015-10-28 12:28:12 -0700

    wlan:Check priviledge permission for SET_THREE_INT_GET_NONE
    Kernel assumes all SET IOCTL commands are assigned with even
    numbers. But in our WLAN driver, some SET IOCTLS are assigned with
    odd numbers. This leads kernel fail to check, for some SET IOCTLs,
    whether user has the right permission to do SET operation.
    Hence, in driver, before processing SET_THREE_INT_GET_NONE IOCTL,
    making sure user task has right permission to process the command.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I3c695160d637ed87b04ccf3299985055a9791c4b
    CRs-Fixed: 930948
    Git-commit: aaeeed43f9597631982835481c7cf2621f6455f0
    Reviewed-on: http://gerrit.mot.com/802786
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    (cherry picked from commit fadf5b7b743012e442104db05fbe90b8733ef2f9)
    Reviewed-on: http://gerrit.mot.com/811994
    Reviewed-by: Navya Vijayan <navya@motorola.com>
    SLTApproved: Harsha Hosakoppa Rudresha <harsha@motorola.com>

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -6753,6 +6753,12 @@ int iw_set_three_ints_getnone(struct net_device *dev,
         return -EBUSY;
     }
 
+    if (!capable(CAP_NET_ADMIN)) {
+        VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+                  FL("permission check failed"));
+        return -EPERM;
+    }
+
     //BEGIN MOT a19110 IKSWL-15774 Ioctl from Mot code
     tmp_value = (int *)extra;
 
