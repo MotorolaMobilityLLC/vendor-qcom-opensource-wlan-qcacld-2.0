c15f64ad4adefc5eadd76e1f2f9efe47d93b3d21
a7301c <a7301c@motorola.com>
2016-10-18 10:49:10 -0500

    Revert ".0: Add INI item to control BPF feature configuration"
    This reverts commit 9efbe5efd84f71a074f6bec985dbd4bcd1110b0c.
    
    Change-Id: Id4e043b330ebe9e15e5a939967b0ed131c2d5dea

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -10446,7 +10446,7 @@ __wlan_hdd_cfg80211_bpf_offload(struct wiphy *wiphy,
 	}
 
 	if (!hdd_ctx->bpf_enabled) {
-		hddLog(LOGE, FL("BPF offload is not supported/enabled"));
+		hddLog(LOGE, FL("BPF offload is not supported by firmware"));
 		return -ENOTSUPP;
 	}
 
